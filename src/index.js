3fb7a566e77b573b13247f4e18b55e5462ace557
260 <reverofyoung@gmail.com>
2023-07-28 15:48:43 +0900

    index.js 수정 (레포지토리 경로 관련)

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import GlobalStyles from './common/GlobalStyles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={ process.env.PUBLIC_URL }>
         <GlobalStyles />
         <App />
     </BrowserRouter>
